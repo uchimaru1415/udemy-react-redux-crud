777f576b47f42039ca5691a5c75c7ba0caeb3c0d
takuma <>
2020-01-26 16:36:53 +0900

    Reactのpropsについて学んだよ。デフォルトのpropsはdefaultPropsで定義できるよ

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from 'react';
 
-// function App () {
-  // return (
-    // <React.Fragment>
-      // <label htmlFor="bar">
-        // bar
-      // </label>
-      // <input type="text" onChange={() =>{
-        // console.log('test')
-      // }} />
-    // </React.Fragment>
-  // );
-// }
 
 const App = () => {
+  const profiles = [
+    {
+      name: 'taro',
+      age: 10
+    },
+    {
+      name: 'jiro',
+      age: 22
+    },
+    {
+      name: 'hanako',
+    }
+  ]
   return (
     <React.Fragment>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User key={index} name={profile.name} age={profile.age} />
+        })
+      }
     </React.Fragment>
   )
 }
 
-const Cat = () => {
+const User = (props) => {
   return (
-    <p>Meow</p>
+    <p>
+      Hi, I am { props.name }
+      I am { props.age } years old
+    </p>
   )
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
