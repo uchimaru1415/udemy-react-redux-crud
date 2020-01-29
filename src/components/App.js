504956b01ae7b92bd7b929275cddfea6d2ecafed
takuma <>
2020-01-29 21:39:16 +0900

    コンポーネント側でreducerを呼び出して状態を変化させたよ

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions'
 
-const App = () => {
-  return (
-    <Counter>
-    </Counter>
-  )
-}
-
-class Counter extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-  handlePlusButton = () => {
-    const count = this.state.count
-    this.setState({ count: count + 1 })
-  }
-  handleMinusButton = () => {
-    const count = this.state.count
-    this.setState({ count: count - 1 })
-  }
+class App extends Component {
   render () {
-    console.log(this.state)
+    const props = this.props
     return (
       <React.Fragment>
-        <div>counter: { this.state.count }</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { value: state.count.value }
+}
+// const mapDispatchToProps = dispatch => ({
+  // increment: () => dispatch(increment()),
+  // decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
